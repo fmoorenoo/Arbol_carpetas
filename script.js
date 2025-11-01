30888124d632406ccfa4ba55cf702d0b66f6e9fa
fmoorenoo <fermontemore@gmail.com>
2025-11-01 16:07:21 +0000

    Estructura html del árbol modificada y script iniciado

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const formAnadir = document.querySelector('#formulario-agregar');
+    const inputNombre = document.querySelector('#nombre');
+    const inputTipo = document.querySelector('#tipo');
+    const inputDestino = document.querySelector('#destino');
+    const inputBusqueda = document.querySelector('#busqueda');
+    const arbol = document.querySelector('#arbol-directorios');
+
+    function esCarpeta(li) {
+        return li.classList.contains('carpeta');
+    }
+
+    function hijosElemento(li) {
+        return li.querySelector(':scope > .hijos');
+    }
+
+    function nombreElemento(li) {
+        return li.getAttribute('data-name');
+    }
+
+    function rutaElemento(li) {
+        return li.getAttribute('data-path');
+    }
 });
